3ec1e6865da414564166ebb73417d12f4b6fa335
Olivier Thierry <olivier.thierry@gmail.com>
2020-05-07 17:39:21 +0200

    Chapitre données dynamiques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MonPremierComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MonPremierComponent, AppareilComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
